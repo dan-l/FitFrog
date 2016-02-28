5e442275dabc984f2f2018249c077378422c4206
Napon Taratan <napon@napontaratan.com>
2016-02-28 05:38:41 -0800

    Almost done plugin

--- fitfroggame/www/js/index.js
+++ fitfroggame/www/js/index.js
@@ -35,6 +35,41 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        navigator.geolocation.getCurrentPosition(function(c){
+            console.log('CALLBACK' + c);
+        });
+        var bgLocationServices =  window.plugins.backgroundLocationServices;
+        bgLocationServices.configure({
+             //Both
+             desiredAccuracy: 10, // Desired Accuracy of the location updates (lower means more accurate but more battery consumption)
+             distanceFilter: 1, // (Meters) How far you must move from the last point to trigger a location update
+             debug: true, // <-- Enable to show visual indications when you receive a background location update
+             interval: 1000, // (Milliseconds) Requested Interval in between location updates.
+             //Android Only
+             notificationTitle: 'BG Plugin', // customize the title of the notification
+             notificationText: 'Tracking', //customize the text of the notification
+             fastestInterval: 500, // <-- (Milliseconds) Fastest interval your app / server can handle updates
+             useActivityDetection: true // Uses Activitiy detection to shut off gps when you are still (Greatly enhances Battery Life)
+
+        });
+
+        //Register a callback for location updates, this is where location objects will be sent in the background
+        bgLocationServices.registerForLocationUpdates(function(location) {
+             console.log("We got an BG Update" + JSON.stringify(location));
+        }, function(err) {
+             console.log("Error: Didnt get an update", err);
+        });
+
+        bgLocationServices.registerForActivityUpdates(function(activities) {
+             console.log("We got an BG Update" + JSON.stringify(activities));
+        }, function(err) {
+             console.log("Error: Something went wrong", err);
+        });
+
+
+
+        bgLocationServices.start();
+
         // console.log(navigator.camera);
         // console.log(Clarifai);
         // navigator.camera.getPicture(function cameraCallback(imageData) {
@@ -49,28 +84,28 @@ var app = {
         //     destinationType: Camera.DestinationType.DATA_URL
         // });
 
-        var options = { frequency: 200 };  // Update every 3 seconds
+        // var options = { frequency: 200 };  // Update every 3 seconds
 
-        var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+        // var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
 
-        function onSuccess(acceleration) {
-            var x = acceleration.x;
-            var y = acceleration.y;
-            var z = acceleration.z;
+        // function onSuccess(acceleration) {
+        //     var x = acceleration.x;
+        //     var y = acceleration.y;
+        //     var z = acceleration.z;
 
-            var mAccelCurrent = Math.sqrt(x*x + y*y + z*z);
-            mAccel = mAccel * 0.9 + mAccelCurrent * 0.1;
-            var element = document.getElementById('accelerometer');
-            element.innerHTML = 'Acceleration X: ' + acceleration.x         + '<br />' +
-                                'Acceleration Y: ' + acceleration.y         + '<br />' +
-                                'Acceleration Z: ' + acceleration.z         + '<br />' +
-                                'mAccel: '      + mAccel + '<br />';
-        }
-        // onError: Failed to get the acceleration
-        //
-        function onError() {
-            alert('onError!');
-        }
+        //     var mAccelCurrent = Math.sqrt(x*x + y*y + z*z);
+        //     mAccel = mAccel * 0.9 + mAccelCurrent * 0.1;
+        //     var element = document.getElementById('accelerometer');
+        //     element.innerHTML = 'Acceleration X: ' + acceleration.x         + '<br />' +
+        //                         'Acceleration Y: ' + acceleration.y         + '<br />' +
+        //                         'Acceleration Z: ' + acceleration.z         + '<br />' +
+        //                         'mAccel: '      + mAccel + '<br />';
+        // }
+        // // onError: Failed to get the acceleration
+        // //
+        // function onError() {
+        //     alert('onError!');
+        // }
 
     },
     // Update DOM on a Received Event
